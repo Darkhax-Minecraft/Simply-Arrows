7d94dd8adb0cb7ba3886dbaa43e94ad0017b4f5f
Tyler Hancock <darklime@live.ca>
2018-01-25 21:42:54 -0700

    Moved mod to right package name. Upped min bookshelf ver.

--- src/main/java/net/darkhax/simplyarrows/SimplyArrows.java
+++ src/main/java/net/darkhax/simplyarrows/SimplyArrows.java
@@ -1,17 +1,17 @@
-package net.darkhax.infoaccessories;
+package net.darkhax.simplyarrows;
 
 import net.darkhax.bookshelf.lib.LoggingHelper;
 import net.darkhax.bookshelf.registry.RegistryHelper;
-import net.minecraft.creativetab.CreativeTabs;
+import net.darkhax.simplyarrows.creativetab.CreativeTabSimplyArrows;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "simplyarrows", name = "Simply Arrows", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.2.462,);", certificateFingerprint = "@FINGERPRINT@")
+@Mod(modid = "simplyarrows", name = "Simply Arrows", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.2.523,);", certificateFingerprint = "@FINGERPRINT@")
 public class SimplyArrows {
 
     public static final LoggingHelper LOG = new LoggingHelper("Simply Arrows");
-    public static final RegistryHelper REGISTRY = new RegistryHelper("simplyarrows").enableAutoRegistration().setTab(CreativeTabs.MISC);
+    public static final RegistryHelper REGISTRY = new RegistryHelper("simplyarrows").enableAutoRegistration().setTab(new CreativeTabSimplyArrows());
 
     @EventHandler
     public void preInit (FMLPreInitializationEvent event) {
